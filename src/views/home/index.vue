9b9fe5ac94fc800f9dea02c222831b62d8ac71b8
Magdalena Siekańska <magdalena.siekanska@gmail.com>
2021-10-19 12:32:25 +0200

    initial commit

---
+++ src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="./images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue3 Boilerplate" />
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/hello-world.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+}
+</script>
