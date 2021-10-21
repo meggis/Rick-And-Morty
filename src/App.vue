ebc91319cac9853a3473ea43f21e35522acc7d11
Magdalena Siekańska <magdalena.siekanska@gmail.com>
2021-10-21 16:24:36 +0200

    add base of pagination content

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <Header />
   <hr />
+  <Pagination
+    :numberOfPages="numberOfPages"
+    @increment="increment"
+    @decrement="decrement"
+    @PaginationOnClick="onLoadPage"
+    @setPageNum="setPageNum"
+  />
   <Home :characters="characters" />
   <div class="layout">
     <Footer />
@@ -12,11 +19,13 @@ import { defineComponent } from '@vue/runtime-core'
 import Footer from './views/shared/Footer.vue'
 import Header from './views/shared/Header.vue'
 import Home from './views/home/home.vue'
+import Pagination from './components/Pagination.vue'
 import axios from 'axios'
 
 export default defineComponent({
   name: 'HomePage',
   components: {
+    Pagination,
     Footer,
     Header,
     Home,
@@ -24,17 +33,83 @@ export default defineComponent({
   data() {
     return {
       characters: [],
+      currentPage: 1,
+      numberOfPages: null,
     }
   },
   created() {
     axios
-      .get('https://rickandmortyapi.com/api/character')
+      .get(`https://rickandmortyapi.com/api/character?page=${this.currentPage}`)
       .then((response) => {
-        console.log(response.data.results)
+        console.log(response.data.info)
         this.characters = response.data.results
+        this.numberOfPages = response.data.info.pages
       })
       .catch((error) => console.log(error))
   },
+  methods: {
+    async increment() {
+      this.currentPage++
+      axios
+        .get(`https://rickandmortyapi.com/api/character?page=${this.currentPage}`)
+        .then((response) => {
+          this.characters = response.data.results
+          this.numberOfPages = response.data.info.pages
+        })
+        .catch((error) => console.log(error))
+    },
+    async decrement() {
+      this.currentPage--
+      axios
+        .get(`https://rickandmortyapi.com/api/character?page=${this.currentPage}`)
+        .then((response) => {
+          this.characters = response.data.results
+          this.numberOfPages = response.data.info.pages
+        })
+        .catch((error) => console.log(error))
+    },
+    setPageNum(num: number): void {
+      this.currentPage = num
+      axios
+        .get(`https://rickandmortyapi.com/api/character?page=${this.currentPage}`)
+        .then((response) => {
+          this.characters = response.data.results
+          this.numberOfPages = response.data.info.pages
+        })
+        .catch((error) => console.log(error))
+    },
+    // isSomeSet(): boolean {
+    //   return !!this.currentPage
+    // },
+    // onCurrentPageClick(currentPage) {
+    //   console.log(currentPage)
+    // },
+    // async pageChangeHandle(value) {
+    //   switch (value) {
+    //     case 'next':
+    //       this.currentPage += 1
+    //       break
+    //     case 'previous':
+    //       this.currentPage -= 1
+    //       break
+    //     default:
+    //       this.currentPage = value
+    //   }
+    //   const { data } = await axios.get(
+    //     `?country=us&page=${this.currentPage}&pageSize=${
+    //       this.$options.static.visibleItemsPerPageCount
+    //     }&category=business&apiKey=065703927c66462286554ada16a686a1`
+    //   )
+    // },
+  },
+  // async mounted() {
+  //   const { data } = await axios.get(
+  //       `https://rickandmortyapi.com/api/character?page=${this.currentPage}`
+  //     ).then(
+  //       this.characters = data.response.data.results
+  //       this.numberOfPages = data.response.data.info.pages
+  //     )
+  // },
 })
 </script>
 
---
+++ src/components/Pagination.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="$emit('decrement')">&lt;</button>
+    <button
+      v-for="numberOfPage in numberOfPages"
+      :key="numberOfPage.id"
+      @click="$emit('setPageNum', numberOfPage)"
+    >
+      {{ numberOfPage }}
+    </button>
+    <button @click="$emit('increment')">&gt;</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    numberOfPages: {
+      type: Number,
+    },
+  },
+  // methods: {
+  //   PaginationOnClick() {
+  //     this.$emit('loadPageClicked', this.numberOfPages)
+  //   },
+  // },
+}
+</script>
