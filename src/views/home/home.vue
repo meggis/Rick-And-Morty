f3627008abad720d26c72b106346e9ee4b283a05
Magdalena Siekańska <magdalena.siekanska@gmail.com>
2021-10-21 12:51:05 +0200

    initial commit

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,36 +1,49 @@
 <template>
-  <div id="table">
-    <!-- {{ character }} -->
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Photo</th>
-          <th scope="col">Character ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Gender</th>
-          <th scope="col">Species</th>
-          <th scope="col">Last Episode</th>
-          <th scope="col">Add To Favorites</th>
-        </tr>
-      </thead>
-      <tbody v-for="character in characters" :key="character.id">
-        <tr>
-          <td class="align-middle">
-            <img class="character-img" :src="character.image" />
-          </td>
-          <td class="align-middle">{{ character.id }}</td>
-          <td class="align-middle">{{ character.name }}</td>
-          <td class="align-middle">{{ character.gender }}</td>
-          <td class="align-middle">{{ character.species }}</td>
-          <td class="align-middle">{{ character.episode.length }}</td>
-          <td class="align-middle">
-            <button type="button" class="btn star-border">
-              <span class="material-icons md-14">star</span>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col">
+        <div class="btn-group" role="group">
+          <button type="button" class="btn">All characters</button>
+          <button type="button" class="btn">Favorites</button>
+        </div>
+        <div id="table">
+          <!-- {{ character }} -->
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Photo</th>
+                <th scope="col">Character ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Species</th>
+                <th scope="col">Last Episode</th>
+                <th scope="col">Add To Favorites</th>
+              </tr>
+            </thead>
+            <tbody v-for="character in characters" :key="character.id">
+              <tr>
+                <td class="align-middle">
+                  <img class="character-img" :src="character.image" />
+                </td>
+                <td class="align-middle">{{ character.id }}</td>
+                <td class="align-middle">{{ character.name }}</td>
+                <td class="align-middle">{{ character.gender }}</td>
+                <td class="align-middle">{{ character.species }}</td>
+                <td class="align-middle">{{ character.episode.length }}</td>
+                <td class="align-middle">
+                  <button type="button" class="btn star-border">
+                    <span class="material-icons md-14">star</span>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div>
+            <ul class="pagination"></ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,9 +80,7 @@ export default defineComponent({
     font-size: 14px;
   }
 }
-
-td,
-th {
-  text-align: center;
+table {
+  color: #a9b1bd;
 }
 </style>
